1ab3c0c0ff1b705d60cc87d0164737c7cf00756d
Samich786 <msami.signup@gmail.com>
2025-03-22 02:03:01 +0500

    pages setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/student/Home"
-
+import CourseDetails from "./pages/student/CourseDetails"
 const App = () => {
   return ( 
     <div>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/" element={<CourseDetails />} />
     </Routes>
     
     
